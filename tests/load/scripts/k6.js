d0207313ccb380a5dd8b18fd88070c3bc383cba1
AndrewReis <andrew-reiis@outlook.com>
2026-01-11 21:57:12 -0300

    Add k6 load testing dashboard and update docker-compose configuration
    - Created a new JSON dashboard for visualizing k6 load testing results using InfluxDB.
    - Updated docker-compose.yml to improve service dependencies and network configurations.
    - Changed the API base URL in k6 load testing script from port 3000 to 3333 for consistency with the updated service configuration.

--- tests/load/scripts/k6.js
+++ tests/load/scripts/k6.js
@@ -12,7 +12,7 @@ export let options = {
 };
 
 export default function () {
-  const BASE_URL = 'http://api:3000/api/v1';
+  const BASE_URL = 'http://api:3333/api/v1';
   const resource = 'shorten/7e20ABk';
 
   const response = http.get(`${BASE_URL}/${resource}`, { headers: { Accepts: "application/json" } });
